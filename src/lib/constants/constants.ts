a322386fbf0f034331d9109628c0696aa9039b87
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-01-15 23:25:19 +0300

    feat: add address schema and interface, and default user avatar constant

---
+++ src/lib/constants/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_USER_AVATAR =
+  'https://res.cloudinary.com/dvp1mjhd9/image/upload/v1714690850/default_profile_image.png';
---
+++ src/lib/shared/address.schema.ts
@@ -0,0 +1,42 @@
+import { Schema } from 'mongoose';
+import { AddressInterface } from './address.interface';
+
+export const AddressSchema: Schema<AddressInterface> =
+  new Schema<AddressInterface>(
+    {
+      city: {
+        type: String,
+        trim: true,
+        required: [true, 'An address must have a city'],
+        minlength: [3, 'A city name must have more or equal then 3 characters'],
+      },
+      subcity: {
+        type: String,
+        trim: true,
+        required: [true, 'An address must have a subcity'],
+        minlength: [
+          3,
+          'A subcity name must have more or equal then 3 characters',
+        ],
+      },
+      woreda: {
+        type: String,
+        trim: true,
+        required: [true, 'An address must have a woreda'],
+        minlength: [
+          3,
+          'A woreda name must have more or equal then 3 characters',
+        ],
+      },
+      street: {
+        type: String,
+        trim: true,
+        required: [true, 'An address must have a street'],
+        minlength: [
+          3,
+          'A street name must have more or equal then 3 characters',
+        ],
+      },
+    },
+    { _id: false }
+  );
