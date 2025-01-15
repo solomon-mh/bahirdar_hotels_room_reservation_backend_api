bcd346d45a097c8bb8ace3fea83f2833ad29e865
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-01-15 23:44:25 +0300

    feat: add password hashing and reset token generation to user model

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,8 @@
-import { IUser } from './interfaces/user.interface';
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
 import mongoose, { Schema, Model, Document } from 'mongoose';
+
+import { IUser } from './interfaces/user.interface';
 import { Gender } from './enums/gender.enum';
 import { UserRole } from './enums/user-role.enum';
 import { DEFAULT_USER_AVATAR } from '../lib/constants/constants';
@@ -71,6 +74,32 @@ const UserSchema: Schema<IUser> = new Schema<IUser>(
     timestamps: true,
   }
 );
+
+// Hash the password before save
+UserSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  const salt = await bcrypt.genSalt(12);
+  const hashedPassword = await bcrypt.hash(this.password as string, salt);
+  this.password = hashedPassword;
+
+  next();
+});
+
+UserSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 const UserModel: Model<IUserModel> = mongoose.model<IUserModel>(
   'User',
