4093b9b433259027f6ca818dd4f8ffdef7767027
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-01-15 23:25:34 +0300

    refactor: replace inline address schema with shared AddressSchema import

--- src/hotels/hotels.model.ts
+++ src/hotels/hotels.model.ts
@@ -3,42 +3,9 @@ import UserModel from '../users/users.model';
 import { Hotel } from './interfaces/hotel.interface';
 import { Document } from 'mongoose';
 import { Schema } from 'mongoose';
-import { AddressInterface } from './interfaces/address.interface';
+import { AddressSchema } from '../lib/shared/address.schema';
 
 export interface IHotelModel extends mongoose.Model<Hotel & Document> {}
-
-const AddressSchema: Schema<AddressInterface> = new Schema<AddressInterface>(
-  {
-    city: {
-      type: String,
-      trim: true,
-      required: [true, 'An address must have a city'],
-      minlength: [3, 'A city name must have more or equal then 3 characters'],
-    },
-    subcity: {
-      type: String,
-      trim: true,
-      required: [true, 'An address must have a subcity'],
-      minlength: [
-        3,
-        'A subcity name must have more or equal then 3 characters',
-      ],
-    },
-    woreda: {
-      type: String,
-      trim: true,
-      required: [true, 'An address must have a woreda'],
-      minlength: [3, 'A woreda name must have more or equal then 3 characters'],
-    },
-    street: {
-      type: String,
-      trim: true,
-      required: [true, 'An address must have a street'],
-      minlength: [3, 'A street name must have more or equal then 3 characters'],
-    },
-  },
-  { _id: false }
-);
 
 const HotelSchema: Schema<Hotel> = new Schema<Hotel>(
   {
