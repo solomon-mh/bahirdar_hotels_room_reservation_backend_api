d48d23dcff6f3518650694feee1550473c10c533
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-03 23:52:12 +0300

    feat: add payment model and interface for booking payments

---
+++ src/payments/interfaces/payment.interface.ts
@@ -0,0 +1,28 @@
+import { Types } from 'mongoose';
+import { Gender } from '../../lib/shared/gender.enum';
+
+export interface IPayment {
+  _id?: Types.ObjectId;
+
+  firstName: string;
+  lastName: string;
+  email: string;
+  dateOfBirth: Date;
+  phoneNumber: string;
+  gender: Gender;
+
+  hotelName: string;
+  summary: string;
+  roomNumber: string;
+  roomType: string;
+  roomFacilities: string[];
+  capacity: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalPrice: number;
+  pricePerNight: number;
+  numOfNights: number;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
---
+++ src/payments/payment.model.ts
@@ -0,0 +1,105 @@
+import mongoose, { Model } from 'mongoose';
+import { IPayment } from './interfaces/payment.interface';
+import { Schema } from 'mongoose';
+import { Gender } from '../lib/shared/gender.enum';
+import validator from 'validator';
+import { RoomType } from '../rooms/enums/room-type.enum';
+
+interface IPaymentMethods {}
+
+export interface IPaymentModel extends Model<IPayment, {}, IPaymentMethods> {}
+
+const PaymentSchema = new Schema<IPayment, IPaymentModel>(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, 'First name is required'],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: [true, 'Last name is required'],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: [true, 'Date of birth is required'],
+    },
+    gender: {
+      type: String,
+      enum: Gender,
+      required: [true, 'Gender is required'],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, 'Email is required'],
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+      required: [true, 'Phone number is required'],
+    },
+    hotelName: {
+      type: String,
+      trim: true,
+      required: [true, 'Hotel name is required'],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Summary is required'],
+    },
+    roomNumber: {
+      type: String,
+      trim: true,
+      required: [true, 'Room number is required'],
+    },
+    roomType: {
+      type: String,
+      trim: true,
+      enum: RoomType,
+      required: [true, 'Room type is required'],
+    },
+    roomFacilities: {
+      type: [String],
+      required: [true, 'Room facilities is required'],
+    },
+    capacity: {
+      type: Number,
+      required: [true, 'Capacity is required'],
+    },
+    checkIn: {
+      type: Date,
+      required: [true, 'Check in date is required'],
+    },
+    checkOut: {
+      type: Date,
+      required: [true, 'Check out date is required'],
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, 'Total price is required'],
+    },
+    pricePerNight: {
+      type: Number,
+      required: [true, 'Price per night is required'],
+    },
+    numOfNights: {
+      type: Number,
+      required: [true, 'Number of nights is required'],
+      min: [1, 'Number of nights must be greater than 0'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PaymentModel = mongoose.model<IPayment, IPaymentModel>(
+  'Payment',
+  PaymentSchema
+);
+
+export default PaymentModel;
