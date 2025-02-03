abb56e89a391d113ee7c3d92cd76944cde1afaf1
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-03 22:57:51 +0300

    feat: add endpoint for verifying saved booking payments

--- src/payments/payments.route.ts
+++ src/payments/payments.route.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import { AuthController } from '../auth/auth.controller';
-import { ChapaService } from './providers/chapa.service';
 import { ChapaController } from './chapa.controller';
 
 const authController = new AuthController();
-
-const chapaService = new ChapaService();
-const chapaController = new ChapaController(chapaService);
+const chapaController = new ChapaController();
 
 const router = express.Router();
 
@@ -17,6 +14,9 @@ router.post('/chapa/accept-booking-payment/:bookingId', (req, res) =>
 );
 router.get('/chapa/verify-booking-payment/:trx_ref', (req, res) =>
   chapaController.verifyBookingPayment(req, res)
+);
+router.get('/chapa/verify-save-booking-payment', (req, res) =>
+  chapaController.verifySaveBookingPayment(req, res)
 );
 
 export const paymentRoutes = router;
