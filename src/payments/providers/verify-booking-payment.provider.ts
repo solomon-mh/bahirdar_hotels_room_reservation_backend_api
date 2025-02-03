bb7d6f125ec49450f3669eb4df834426078684ca
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-03 22:32:21 +0300

    feat: add provider for verifying booking payments with Chapa integration

---
+++ src/payments/providers/verify-booking-payment.provider.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+
+export async function verifyBookingPaymentProvider(
+  req: Request,
+  res: Response
+) {
+  console.log('verify booking payment...');
+  try {
+    const { trx_ref } = req.params;
+
+    const myHeaders = new Headers();
+    myHeaders.append('Authorization', `Bearer ${process.env.CHAPA_API_KEY}`);
+    const requestOptions: any = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+
+    const response = await fetch(
+      `https://api.chapa.co/v1/transaction/verify/${trx_ref}`,
+      requestOptions
+    );
+
+    if (!response.ok) {
+      console.log('network error');
+    }
+
+    const data = await response.json();
+
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+      message: (err as Error).message,
+    });
+  }
+}
