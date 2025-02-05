877db84bb997064066134dbf6393cc871720861a
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-05 10:46:07 +0300

    feat: add utility function for pagination data retrieval

---
+++ src/lib/utils/get-pagination-data.util.ts
@@ -0,0 +1,15 @@
+import { Model } from 'mongoose';
+
+export async function getPaginationData<T>(
+  limit: string | number | undefined,
+  page: string | number | undefined,
+  model: Model<T>
+) {
+  const _limit = limit ? +limit : 20;
+  const _page = page ? +page : 1;
+  const skip = (_page - 1) * _limit;
+  const totalProducts = await model.countDocuments();
+  const totalPages = Math.ceil(totalProducts / _limit);
+
+  return { skip, _limit, totalPages, _page };
+}
