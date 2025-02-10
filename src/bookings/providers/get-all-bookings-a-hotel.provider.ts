71cec6e4af53fcd922a7d2c09516f9c72715e3de
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-10 22:45:58 +0300

    feat: add pagination on get all bookings by hotel id

--- src/bookings/providers/get-all-bookings-a-hotel.provider.ts
+++ src/bookings/providers/get-all-bookings-a-hotel.provider.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import BookingModel from '../bookings.model';
 import { Types } from 'mongoose';
 import HotelModel from '../../hotels/hotels.model';
+import { IGetAllQuery } from '../../lib/shared/get-all-query.interface';
+import { getPaginationDataUtil } from '../../lib/utils/get-pagination-data.util';
 
 export async function getAllBookingsOfAHotelProvider(
   req: Request,
@@ -10,6 +12,7 @@ export async function getAllBookingsOfAHotelProvider(
   console.log('get all bookings of a hotel...');
   try {
     const { hotelId } = req.params;
+    const { limit, page } = req.query as IGetAllQuery;
 
     const hotel = await HotelModel.findById(hotelId);
 
@@ -20,6 +23,14 @@ export async function getAllBookingsOfAHotelProvider(
       });
       return;
     }
+
+    // pagination
+    const { skip, _limit, totalPages, _page } = await getPaginationDataUtil(
+      limit,
+      page,
+      BookingModel,
+      { hotel: new Types.ObjectId(hotelId) }
+    );
 
     const bookings = await BookingModel.aggregate([
       {
@@ -49,14 +60,28 @@ export async function getAllBookingsOfAHotelProvider(
       {
         $unwind: '$user',
       },
+
+      // pagination
+      {
+        $sort: { createdAt: -1 },
+      },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: _limit,
+      },
     ]);
 
     res.status(200).json({
       status: 'success',
-      data: {
-        hotel,
-        bookings,
+      pagination: {
+        totalPages,
+        page: _page,
+        limit: _limit,
       },
+      hotel,
+      data: bookings,
     });
   } catch (err) {
     console.log(err);
