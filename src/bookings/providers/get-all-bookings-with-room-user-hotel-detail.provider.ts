84852b766d73f1defacfce0ec7a3ad9433128b15
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-11 10:12:40 +0300

    feat: add filtering and pagination on getAllBookingsWithRoomUserHotelDetailProvider

--- src/bookings/providers/get-all-bookings-with-room-user-hotel-detail.provider.ts
+++ src/bookings/providers/get-all-bookings-with-room-user-hotel-detail.provider.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import BookingModel from '../bookings.model';
 import { UserRole } from '../../users/enums/user-role.enum';
 import { Types } from 'mongoose';
+import { IGetAllBookingQuery } from '../interfaces/get-all-bookings.interface';
+import { getPaginationDataUtil } from '../../lib/utils/get-pagination-data.util';
 
 export async function getAllBookingsWithRoomUserHotelDetailProvider(
   req: Request,
@@ -11,19 +13,51 @@ export async function getAllBookingsWithRoomUserHotelDetailProvider(
   try {
     const account = req.user;
 
-    let filterObj: Record<string, any> = {};
+    const { isPaid, room, status, numOfNight, paymentDate, limit, page } =
+      req.query as IGetAllBookingQuery;
+
+    let filter: Record<string, any> = {};
 
     if (account.role !== UserRole.ADMIN) {
-      filterObj.hotel = new Types.ObjectId(account.hotel);
+      filter.hotel = new Types.ObjectId(account.hotel);
     }
 
-    const bookingsWithDetail = await BookingModel.find(filterObj)
-      .populate('room')
-      .populate('user')
-      .populate('hotel');
+    if (room) filter.room = new Types.ObjectId(room);
+    if (isPaid) filter.isPaid = isPaid;
+    if (status) filter.status = status;
+    if (numOfNight) filter.numOfNight = numOfNight;
+    if (paymentDate) filter.paymentDate = { $gte: new Date(paymentDate) };
+
+    // pagination
+    const { skip, _limit, totalPages, _page } = await getPaginationDataUtil(
+      limit,
+      page,
+      BookingModel
+    );
+
+    const bookingsWithDetail =
+      account.role === UserRole.ADMIN
+        ? await BookingModel.find(filter)
+            .sort({ updatedAt: -1 })
+            .skip(skip)
+            .limit(_limit)
+            .populate('room')
+            .populate('user')
+            .populate('hotel')
+        : await BookingModel.find(filter)
+            .sort({ updatedAt: -1 })
+            .skip(skip)
+            .limit(_limit)
+            .populate('room')
+            .populate('user');
 
     res.status(200).json({
       status: 'success',
+      pagination: {
+        totalPages,
+        limit: _limit,
+        page: _page,
+      },
       data: bookingsWithDetail,
     });
   } catch (err) {
