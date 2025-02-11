092b9f110ada41d11d09498a7b0f540efc1c89fc
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-11 12:25:46 +0300

    feat: update booking status enum to include REJECTED and remove PAID from payment verification

--- src/bookings/enums/booking-status.enum.ts
+++ src/bookings/enums/booking-status.enum.ts
@@ -1,9 +1,11 @@
 export enum BookingStatus {
   PENDING = 'pending',
+
   CONFIRMED = 'confirmed',
+  REJECTED = 'rejected',
+
   CANCELLED = 'cancelled',
+
   CHECKED_IN = 'checked-in',
   CHECKED_OUT = 'checked-out',
-
-  PAID = 'paid',
 }
--- src/payments/providers/verify-save-booking-payent.provider.ts
+++ src/payments/providers/verify-save-booking-payent.provider.ts
@@ -56,7 +56,6 @@ export async function verifySaveBookingPaymentProvider(
         {
           isPaid: true,
           paymentDate: new Date(),
-          status: BookingStatus.PAID,
         },
         {
           new: true,
