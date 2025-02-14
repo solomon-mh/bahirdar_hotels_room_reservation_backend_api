509fd166daf97a101f725cd8feb3bcb2a753c74a
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-14 17:11:58 +0300

    feat: swagger set up

---
+++ src/swagger.ts
@@ -0,0 +1,64 @@
+import { Express, Request, Response } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { version } from '../package.json';
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hotel Booking App Backend API',
+      version,
+      summary: 'summary of our api',
+      description: 'description of the application',
+      contact: {
+        name: 'Edmealem',
+        email: 'edmealemkassahun@gmail.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+        // description: 'run you app locally and test it',
+      },
+      {
+        url: 'http://backend_url.api.com',
+        // description: 'deployed version of our application',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: [
+    './src/**/*.route.ts',
+    './src/**/*.interface.ts',
+    './src/**/*.docs.yml',
+  ],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app: Express, port: number) {
+  // Swagger page
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // Docs in JSON format
+  app.get('/api-docs.json', (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+}
+
+export default swaggerDocs;
