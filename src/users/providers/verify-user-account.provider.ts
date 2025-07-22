30ab85f3a334e1800e084af8b5cb9a656c0978b0
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-22 05:36:13 +0300

    fix: standardize string quotes in verify user account provider

--- src/users/providers/verify-user-account.provider.ts
+++ src/users/providers/verify-user-account.provider.ts
@@ -1,43 +1,45 @@
-import { Request, Response } from 'express';
-import UserModel from '../users.model';
-import { IUser } from '../interfaces/user.interface';
+import { Request, Response } from "express";
+import UserModel from "../users.model";
+import { IUser } from "../interfaces/user.interface";
 
 export async function verifyUserAccountProvider(req: Request, res: Response) {
-  console.log('verify user provider...');
+  console.log("verify user provider...");
   try {
     const admin = req.user;
+    if (!admin) throw new Error("Admin not found");
+
     const { id } = req.params;
 
     const user = (await UserModel.findById(id)) as any as IUser;
 
     if (!user) {
       res.status(400).json({
-        status: 'fail',
-        message: 'No user found when verifying',
+        status: "fail",
+        message: "No user found when verifying",
       });
       return;
     }
 
     if (!user.isVerificationRequested) {
       res.status(400).json({
-        status: 'fail',
-        message: 'User has not requested for verification',
+        status: "fail",
+        message: "User has not requested for verification",
       });
       return;
     }
 
     if (user.isOnboarding) {
       res.status(400).json({
-        status: 'fail',
-        message: 'User has not completed onboarding',
+        status: "fail",
+        message: "User has not completed onboarding",
       });
       return;
     }
 
     if (user.isVerified) {
       res.status(400).json({
-        status: 'fail',
-        message: 'User is already verified',
+        status: "fail",
+        message: "User is already verified",
       });
       return;
     }
@@ -54,14 +56,14 @@ export async function verifyUserAccountProvider(req: Request, res: Response) {
     );
 
     res.status(200).json({
-      status: 'success',
-      message: 'User verified successfully',
+      status: "success",
+      message: "User verified successfully",
       data: updatedUser,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      status: 'error',
+      status: "error",
       message: (err as Error).message,
     });
   }
