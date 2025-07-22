a47abf4e415b83236e55237e15847cb2c036a8dc
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-22 05:36:07 +0300

    fix: standardize string quotes in user with favorites provider

--- src/users/providers/user-with-favorites.provider.ts
+++ src/users/providers/user-with-favorites.provider.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from 'express';
-import UserModel from '../users.model';
-import { Types } from 'mongoose';
+import { Request, Response } from "express";
+import UserModel from "../users.model";
+import { Types } from "mongoose";
 
 export async function getUserWithFavoritesProvider(
   req: Request,
   res: Response
 ) {
-  console.log('get user with favorites...');
+  console.log("get user with favorites...");
   try {
-    const userId = req.user._id!;
+    const userId = req.user?._id!;
 
     const data = await UserModel.aggregate([
       // find user by id
@@ -16,23 +16,23 @@ export async function getUserWithFavoritesProvider(
       // lookup favorites
       {
         $lookup: {
-          from: 'favorites',
-          localField: '_id',
-          foreignField: 'user',
-          as: 'favorites',
+          from: "favorites",
+          localField: "_id",
+          foreignField: "user",
+          as: "favorites",
         },
       },
     ]);
 
     res.status(200).json({
-      status: 'success',
-      message: 'User with favorites',
+      status: "success",
+      message: "User with favorites",
       data: data[0],
     });
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      status: 'error',
+      status: "error",
       message: (err as Error).message,
     });
   }
