190c6913ac042b617da5e189825008239d3275c5
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-22 05:33:31 +0300

    fix: format code style and import passport for user authentication

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { StatusCodes } from 'http-status-codes';
-import { userRoutes } from './users/users.route';
-import { hotelRoutes } from './hotels/hotels.route';
-import { roomRoutes } from './rooms/rooms.route';
-import { bookingRoutes } from './bookings/bookings.route';
-import { reviewRoutes } from './review/review.route';
-import { authRoutes } from './auth/auth.route';
-import { favoritesRoutes } from './favorites/favorites.route';
-import { paymentRoutes } from './payments/payments.route';
-import { statRoutes } from './stats/stats.route';
-import { recommendationRoutes } from './recommendations/recommendations.route';
-import swaggerDocs from './swagger';
-import { envConfig } from './lib/config/environment.config';
+import express, { Request, Response } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+import { userRoutes } from "./users/users.route";
+import { hotelRoutes } from "./hotels/hotels.route";
+import { roomRoutes } from "./rooms/rooms.route";
+import { bookingRoutes } from "./bookings/bookings.route";
+import { reviewRoutes } from "./review/review.route";
+import { authRoutes } from "./auth/auth.route";
+import { favoritesRoutes } from "./favorites/favorites.route";
+import { paymentRoutes } from "./payments/payments.route";
+import { statRoutes } from "./stats/stats.route";
+import { recommendationRoutes } from "./recommendations/recommendations.route";
+import swaggerDocs from "./swagger";
+import { envConfig } from "./lib/config/environment.config";
+import "../src/lib/config/passport";
+import passport from "passport";
 
 export function createApp() {
   const app = express();
@@ -22,9 +24,9 @@ export function createApp() {
   app.use(
     cors({
       origin: [
-        'https://hotel-booking-app-frontend-seven.vercel.app',
-        'http://localhost:5173',
-        'http://localhost:8081',
+        "https://hotel-booking-app-frontend-seven.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:8081",
       ],
       credentials: true,
     })
@@ -34,32 +36,35 @@ export function createApp() {
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
 
+  // initialize passport
+  app.use(passport.initialize());
+
   // Serving static files
   app.use(express.static(path.resolve(__dirname, `./../public`)));
 
   // test route
-  app.use('/test', (req, res) => {
-    res.status(StatusCodes.OK).json({ message: 'Hello World' });
+  app.use("/test", (req, res) => {
+    res.status(StatusCodes.OK).json({ message: "Hello World" });
   });
 
   // all routes
-  app.use('/api/v1/users', userRoutes);
-  app.use('/api/v1/hotels', hotelRoutes);
-  app.use('/api/v1/rooms', roomRoutes);
-  app.use('/api/v1/bookings', bookingRoutes);
-  app.use('/api/v1/reviews', reviewRoutes);
-  app.use('/api/v1/auth', authRoutes);
-  app.use('/api/v1/favorites', favoritesRoutes);
-  app.use('/api/v1/payments', paymentRoutes);
-  app.use('/api/v1/stats', statRoutes);
-  app.use('/api/v1/recommendations', recommendationRoutes);
+  app.use("/api/v1/users", userRoutes);
+  app.use("/api/v1/hotels", hotelRoutes);
+  app.use("/api/v1/rooms", roomRoutes);
+  app.use("/api/v1/bookings", bookingRoutes);
+  app.use("/api/v1/reviews", reviewRoutes);
+  app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1/favorites", favoritesRoutes);
+  app.use("/api/v1/payments", paymentRoutes);
+  app.use("/api/v1/stats", statRoutes);
+  app.use("/api/v1/recommendations", recommendationRoutes);
 
   swaggerDocs(app, envConfig.PORT);
 
   // not found route
-  app.all('*', (req: Request, res: Response) => {
+  app.all("*", (req: Request, res: Response) => {
     res.status(404).json({
-      status: 'fail',
+      status: "fail",
       message: `Can't find ${req.originalUrl} on this server!`,
     });
   });
@@ -68,7 +73,7 @@ export function createApp() {
   app.use((err: Error, req: Request, res: Response, next: any) => {
     console.error(err.stack);
     res.status(500).json({
-      status: 'error',
+      status: "error",
       message: (err as Error).message,
     });
   });
