0ec21aa531bcba1031fa7bee2fa25c147ab64e58
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-22 05:35:42 +0300

    fix: standardize string quotes in current user provider

--- src/users/providers/current-user.provider.ts
+++ src/users/providers/current-user.provider.ts
@@ -1,14 +1,15 @@
-import { Request, Response } from 'express';
-import { UsersService } from './users.service';
-import { UserRole } from '../enums/user-role.enum';
-import UserModel from '../users.model';
+import { Request, Response } from "express";
+import { UsersService } from "./users.service";
+import { UserRole } from "../enums/user-role.enum";
+import UserModel from "../users.model";
 
 const usersService = new UsersService();
 
 export async function getCurrentUserProvider(req: Request, res: Response) {
-  console.log('get current user...');
+  console.log("get current user...");
   try {
     const currentUser = req.user;
+    if (!currentUser) throw new Error("Current User not found");
 
     let user;
 
@@ -21,18 +22,18 @@ export async function getCurrentUserProvider(req: Request, res: Response) {
       currentUser.role === UserRole.CASHIER ||
       currentUser.role === UserRole.MANAGER
     ) {
-      user = await UserModel.findById(currentUser._id!).populate('hotel');
+      user = await UserModel.findById(currentUser._id!).populate("hotel");
     }
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: user,
     });
   } catch (err) {
     res.status(500).json({
-      status: 'error',
+      status: "error",
       message: (err as Error).message,
-      description: 'Error getting current user',
+      description: "Error getting current user",
     });
   }
 }
